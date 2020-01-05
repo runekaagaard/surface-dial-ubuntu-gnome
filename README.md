9453433387567d7dd6632df1bcbe123ff2333113
Rune Kaagaard <rumi.kg@gmail.com>
2020-01-05 21:45:19 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# surface-dial-ubuntu-gnome
-Not done yet! Use the Microsoft Surface Dial on Ubuntu Linux under Gnome
+# Welcome to surface-dial-ubuntu-gnome
+
+Work in progress. It's currently hardcoded to the following events:
+
+- rotate counter clockwise: Mouse scroll up
+- rotate clockwise: Mouse scroll down
+- pressed/released: Mouse pressed/released 
+
+# Usage
+
+You need to install version 1.2.0 of `python-evdev` or higher. Unfortunately,
+most distributions seem to package 1.1.2, so you might have to force an update.
+
+```bash
+$ sudo pip3 install -U evdev
+```
+
+Make sure it's paired via bluetooth and then simply run:
+
+```bash
+$ sudo ./surface-dial-ubuntu-gnome.py
+```
+
+## Acknowledgements
+
+Based of https://github.com/linux-surface/surface-fix-eraser by @StollD.
---
+++ surface-dial-ubuntu-gnome.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import os
+import os.path
+import sys
+
+from errno import ENOENT
+from evdev import InputDevice, UInput
+from evdev.events import InputEvent
+from evdev.ecodes import EV_KEY, EV_REL, REL_WHEEL, BTN_MOUSE
+from signal import signal, SIGINT
+
+# Automatically discover the device.
+for name in os.listdir('/dev/input'):
+    if not name.startswith('event'):
+        continue
+
+    dev = InputDevice('/dev/input/' + name)
+    if dev.name == "Surface Dial System Multi Axis":
+        print("Found surface dial at", dev.path)
+        break
+else:
+    print("Could not find Surface Dial, please pair it and try again.")
+    sys.exit(-ENOENT)
+
+
+def on_exit(s, f):
+    dev.ungrab()
+    sys.exit(0)
+
+
+# Take over complete control over the device - no events will be passed to
+# other listeners. They are relayed to the virtual stylus created above.
+signal(SIGINT, on_exit)
+dev.grab()
+
+cap = {
+    EV_REL: [REL_WHEEL],
+    EV_KEY: [BTN_MOUSE],
+}
+
+
+def write(ui, event, _type, code, value):
+    ui.write_event(InputEvent(event.sec, event.usec, _type, code, value))
+    ui.syn()
+
+
+try:
+    with UInput(cap) as ui:
+        for event in dev.read_loop():
+            if (event.code == 7 and event.type == EV_REL and event.value < 0):
+                print("TURN COUNTER CLOCKWISE")
+                if event.value != -1:
+                    print("    UNEXPECTED COUNTER CLOCKWISE VALUE",
+                          event.value)
+                write(ui, event, EV_REL, REL_WHEEL, 1)
+
+            elif (event.code == 7 and event.type == EV_REL
+                  and event.value > 0):
+                print("TURN CLOCKWISE")
+                if event.value != 1:
+                    print("    UNEXPECTED CLOCKWISE VALUE", event.value)
+                write(ui, event, EV_REL, REL_WHEEL, -1)
+            elif (event.code == 256 and event.type == EV_KEY
+                  and event.value == 1):
+                print("CLICK DOWN")
+                write(ui, event, EV_KEY, BTN_MOUSE, 1)
+            elif (event.code == 256 and event.type == EV_KEY
+                  and event.value == 0):
+                print("CLICK UP")
+                write(ui, event, EV_KEY, BTN_MOUSE, 0)
+finally:
+    dev.ungrab()
