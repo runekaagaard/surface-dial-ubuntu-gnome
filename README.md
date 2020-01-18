bb86a24c7561201d4de752701941223971f0c563
Rune Kaagaard <rumi.kg@gmail.com>
2020-01-18 14:21:06 +0100

    Added dev branch.

--- README.md
+++ README.md
@@ -13,6 +13,11 @@ most distributions seem to package 1.1.2, so you might have to force an update.
 
 ```bash
 $ sudo pip3 install -U evdev
+$ sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0
+$ sudo apt install gir1.2-appindicator3-0.1
+$ sudo apt install libglib2.0-dev libgtk2.0-dev gtk+-3.0
+sudo apt-get install libgtk-3-dev
+sudo apt install python3-tk
 ```
 
 Make sure it's paired via bluetooth and then simply run:
--- surface-dial-ubuntu-gnome.py
+++ surface-dial-ubuntu-gnome.py
@@ -7,7 +7,7 @@ import sys
 from errno import ENOENT
 from evdev import InputDevice, UInput
 from evdev.events import InputEvent
-from evdev.ecodes import EV_KEY, EV_REL, REL_WHEEL, BTN_MOUSE
+from evdev.ecodes import EV_KEY, EV_REL, REL_WHEEL, REL_WHEEL_HI_RES, BTN_MOUSE
 from signal import signal, SIGINT
 
 # Automatically discover the device.
@@ -35,7 +35,7 @@ signal(SIGINT, on_exit)
 dev.grab()
 
 cap = {
-    EV_REL: [REL_WHEEL],
+    EV_REL: [REL_WHEEL, REL_WHEEL_HI_RES],
     EV_KEY: [BTN_MOUSE],
 }
 
@@ -53,18 +53,18 @@ try:
                 if event.value != -1:
                     print("    UNEXPECTED COUNTER CLOCKWISE VALUE",
                           event.value)
-                write(ui, event, EV_REL, REL_WHEEL, 1)
+                write(ui, event, EV_REL, REL_WHEEL_HI_RES, event.value*120)
 
             elif (event.code == 7 and event.type == EV_REL
                   and event.value > 0):
                 print("TURN CLOCKWISE")
                 if event.value != 1:
-                    print("    UNEXPECTED CLOCKWISE VALUE", event.value)
-                write(ui, event, EV_REL, REL_WHEEL, -1)
+                    print("    UNEXPECTED CLOCKWISE VALUE", event.value*120)
+                write(ui, event, EV_REL, REL_WHEEL_HI_RES, event.value)
             elif (event.code == 256 and event.type == EV_KEY
                   and event.value == 1):
                 print("CLICK DOWN")
-                write(ui, event, EV_KEY, BTN_MOUSE, 1)
+                write(ui, event, EV_KEY, BTN_MOUSE, event.value)
             elif (event.code == 256 and event.type == EV_KEY
                   and event.value == 0):
                 print("CLICK UP")
